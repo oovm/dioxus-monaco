bd44cdbefaeb358c45dabcc399c189f2e6510823
Aster <192608617@qq.com>
2022-04-02 09:49:25 +0800

    Add benchmark

--- projects/power-mod/src/lib.rs
+++ projects/power-mod/src/lib.rs
@@ -2,81 +2,53 @@ mod errors;
 
 pub use errors::{Error, Result};
 use num::{
-    bigint::{BigInt, ToBigInt},
+    bigint::BigInt,
     traits::{One, Zero},
-    BigUint, Bounded, Num,
+    Bounded, Num,
 };
 use std::ops::Shr;
 
-pub fn power_mod_fast<T>(base: T, exponent: T, modulus: T) -> T
+/// Fast modular exponentiation for copy type.
+pub fn power_mod_fast<A>(a: A, e: A, m: A) -> A
 where
-    T: Copy + PartialOrd + Num + Bounded,
-    T: Shr<T, Output = T>,
+    A: Copy + PartialOrd + Num + Bounded,
+    A: Shr<A, Output = A>,
 {
-    let zero: T = Zero::zero();
-    let one: T = One::one();
-    let two: T = one + one;
-    let max: T = Bounded::max_value();
-    assert!(modulus != zero);
-    assert!((modulus - one) < (max / (modulus - one)));
+    let zero: A = Zero::zero();
+    let one: A = One::one();
+    let two: A = one + one;
+    let max: A = Bounded::max_value();
+    assert!(m != zero);
+    assert!((m - one) < (max / (m - one)));
 
     let mut result = one;
-    let mut base = base % modulus;
-    let mut exponent = exponent;
+    let mut base = a % m;
+    let mut exponent = e;
 
     loop {
         if exponent <= zero {
             break;
         }
-
         if exponent % two == one {
-            result = (result * base) % modulus;
+            result = (result * base) % m;
         }
-
         exponent = exponent >> one;
-        base = (base * base) % modulus;
+        base = (base * base) % m;
     }
-
     result
 }
 
-fn power_mod<A, E, M>(a: &A, e: &E, m: &M) -> BigInt
+/// Power mod for all integer
+pub fn power_mod<A, E, M>(a: &A, e: &E, m: &M) -> BigInt
 where
     A: Into<BigInt> + Clone,
     E: Into<BigInt> + Clone,
     M: Into<BigInt> + Clone,
 {
     let zero: BigInt = Zero::zero();
-    let one: BigInt = One::one();
-
     let a: BigInt = a.clone().into();
     let e: BigInt = e.clone().into();
     let m: BigInt = m.clone().into();
     assert!(e >= zero);
-    if e == zero {
-        return one;
-    }
-    let mut result = one;
-    let mut base = a % &m;
-    let mut exp = e;
-    loop {
-        if &exp % 2 == &one {
-            result *= &base;
-            result %= &m;
-        }
-
-        if exp == one {
-            return result;
-        }
-
-        exp /= 2;
-        base *= base.clone();
-        base %= &m;
-    }
-}
-
-#[test]
-fn test() {
-    let out = power_mod(&5, &3, &13);
-    print!("{}", out);
+    a.modpow(&e, &m)
 }
